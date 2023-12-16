9d9c393821ea92248e1d6edb52f9f6d4f2c78a6c
Mithun9661 <153515460+Mithun9661@users.noreply.github.com>
2023-12-16 11:59:28 +0530

    #include<stdio.h> int main() {     int x,y,b,a;     scanf("%d%d",&x,&y);      b=(5*x);     if(b>=y){         printf("YES");
        }
        else{
            printf("NO");
        }
        return 0;
    }

---
+++ Reach_Home.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int x,y,b,a;
+    scanf("%d%d",&x,&y);
+     b=(5*x);
+    if(b>=y){
+        printf("YES");
+        
+    }
+    else{
+        printf("NO");
+    }
+    return 0;
+}
