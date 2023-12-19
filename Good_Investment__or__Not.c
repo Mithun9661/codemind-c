99101b6896ef21ed2ddd229f3503b8ed8f3b4256
Mithun9661 <153515460+Mithun9661@users.noreply.github.com>
2023-12-19 11:13:11 +0530

    #include<stdio.h> int main() {     int x,y,z;     scanf("%d%d",&x,&y);     z=2*y;     if(x>=z)     {         printf("YES");
        }
        else{
            printf("NO");
        }
        return 0;
    }

---
+++ Good_Investment__or__Not.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int x,y,z;
+    scanf("%d%d",&x,&y);
+    z=2*y;
+    if(x>=z)
+    {
+        printf("YES");
+        
+    }
+    else{
+        printf("NO");
+    }
+    return 0;
+}
