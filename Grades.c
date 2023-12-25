87487d439bee53c0ed2508fa1de52ed4ac2cf811
Mithun9661 <153515460+Mithun9661@users.noreply.github.com>
2023-12-25 10:16:52 +0530

    #include<stdio.h> int main() {     int p,c,b,m,cs;     scanf("%d%d%d%d%d",&p,&c,&b,&m,&cs);     int x=(p+c+b+m+cs)/5;     if(x>=90)     {         printf("Grade A");     }     else if(x>=80)     {         printf("Grade B");     }     else if(x>=70)     {         printf("Grade C");     }     else if(x>=60)     {         printf("Grade D");     }     else if(x>=50)     {         printf("Grade E");     }     else     {         printf("Grade F");     }     return 0; }

---
+++ Grades.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main()
+{
+    int p,c,b,m,cs;
+    scanf("%d%d%d%d%d",&p,&c,&b,&m,&cs);
+    int x=(p+c+b+m+cs)/5;
+    if(x>=90)
+    {
+        printf("Grade A");
+    }
+    else if(x>=80)
+    {
+        printf("Grade B");
+    }
+    else if(x>=70)
+    {
+        printf("Grade C");
+    }
+    else if(x>=60)
+    {
+        printf("Grade D");
+    }
+    else if(x>=50)
+    {
+        printf("Grade E");
+    }
+    else
+    {
+        printf("Grade F");
+    }
+    return 0;
+}
