194e4c83508016c95a5bd84f2c9cc8920fa7fb6f
Mithun9661 <153515460+Mithun9661@users.noreply.github.com>
2023-12-31 22:24:11 +0530

    #include<stdio.h> int main() {     int x,i;     scanf("%d",&x);     for(i=0;i<=x;i++)     {         if(i%2!=0)         {             printf("%d ",i);         }     }     return 0; }

---
+++ Odd Natural_Numbers.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int x,i;
+    scanf("%d",&x);
+    for(i=0;i<=x;i++)
+    {
+        if(i%2!=0)
+        {
+            printf("%d ",i);
+        }
+    }
+    return 0;
+}
